503df26e2b7485a40ee2075d3a5fe642c43a6b3d
janteter <josephziller8965@gmail.com>
2025-10-26 14:43:43 -0700

    modified button such that it now contains all css, no longer held in homeNavBar, added path to home button and added a home button to the nav bar

--- frontend/src/pages/AddSend.jsx
+++ frontend/src/pages/AddSend.jsx
@@ -1,10 +1,31 @@
 import './AddSend.css'
+import MyButton from  '../components/MyButton.jsx'
+import HomeNavigationBar from '../components/HomeNavigationBar.jsx'
 
 export default function AddSend(){
 
     return (
         <>
-        <h1>This is where you will add a send!</h1>
+        <div className="banner"><HomeNavigationBar></HomeNavigationBar> </div>
+        <h1>This is where you add a send!</h1>
+        <div>
+            <p>
+                Please add information about your send below!
+            </p>
+        </div>
+        <div>
+            <form id="newSend">
+                <label for="style">Send Style <br/></label>
+                <input type="text" id="style"></input><br/><br/>
+                <label for="diff">Send Send Difficulty <br/></label>
+                <input type="text" id="diff"></input><br/><br/>
+                <label for="hold">Send Hold Types <br/></label>
+                <input type="text" id="hold"></input><br/><br/>
+                <MyButton text="Add to Catalogue"></MyButton>
+
+            </form>
+        </div>
+
         </>
     );
 }
