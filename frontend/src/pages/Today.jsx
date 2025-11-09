9d54016285949ee55017bfe804e0ac4e352026e4
janteter <josephziller8965@gmail.com>
2025-11-09 10:02:19 -0800

    Got Today's Sends working! Got the base functionality working but need to add a way to delete climbs from the previous climbs page!

--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -1,6 +1,7 @@
 import "./Today.css"
 import { useEffect, useState } from 'react'
 import { retrieveClimbs } from '../api/api.js';
+import HomeNavigationBar from '../components/HomeNavigationBar.jsx'
 
 function dateFormat() {
         const currDate = new Date();
@@ -19,13 +20,14 @@ export default function TodaySends() {
         let cancelled = false;
         
         const fetchClimbs = async () => {
-            try{
-                setCurrDate(dateFormat())
-                const response = await retrieveClimbs(currData)
+            
+            const currDate = dateFormat()
+            const dateObj = {
+                target_date: currDate
             }
-            catch (error) {
-                alert('Failed to retrieve climb data')
-            }
+
+            console.log(dateObj)
+            const response = await retrieveClimbs(dateObj)
             
             if(!cancelled) {
                 setSendsData(response)
@@ -41,8 +43,17 @@ export default function TodaySends() {
         }, []
     );
 
+    const displaySendList = sendsData.map((climb) => 
+        <div className="list" key={climb.sequence}>
+            <p className="result" >Style:  <br/>{climb.style}</p>
+            <p className="result" >Difficulty: <br/>{climb.difficulty}</p>
+            <p className="result" >Type of Holds: <br/>{climb.holds}</p>
+        </div>
+    )
 
     return <>
-
+        <div><HomeNavigationBar></HomeNavigationBar></div>
+        <h1> Today's Sends</h1>
+        <div> {sendsData && displaySendList} </div>
     </>
 }
