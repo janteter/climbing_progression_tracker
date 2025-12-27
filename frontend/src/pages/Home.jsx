5eda9e7b687a360abde0bcae8cae5ef73659bd55
janteter <josephziller8965@gmail.com>
2025-12-26 22:48:52 -0800

    working on frontend appearance

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,15 +5,17 @@ export default function Home() {
 
     return(
         <>
-        <h1> Climbing Progression Tracker </h1>
-        <div>
-            <p className="myIntro">
-                Welcome to the Climbing Progression Tracker <br/>
-                This application allows you to record, add, and review your climbing ascents! <br/>
-            </p>
-        </div>
-        <div> 
-            <HomeNavigationBar></HomeNavigationBar> 
+        <div class="page-container">
+            <h1 class="title"> Climbing Progression Tracker </h1>
+            <div>
+                <p className="myIntro">
+                    Welcome to the Climbing Progression Tracker <br/>
+                    This application allows you to record, add, and review your climbing ascents! <br/>
+                </p>
+            </div>
+            <div> 
+                <HomeNavigationBar></HomeNavigationBar> 
+            </div>
         </div>
         </>
 
