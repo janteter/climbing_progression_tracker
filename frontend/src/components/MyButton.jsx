d1075f0542861d3fcf84dfee38c6aafd8fddd2ee
janteter <josephziller8965@gmail.com>
2026-01-10 21:51:11 -0800

    migrating to tailwindcss from vanilla css, working on updating a climb using a MUI drawer

--- frontend/src/components/MyButton.jsx
+++ frontend/src/components/MyButton.jsx
@@ -3,7 +3,12 @@ import './MyButton.css';
 export default function MyButton({ text, onClick }) {
 
     return(
-        <button className="custom-b" onClick={onClick}>{text}</button>
+        <button className="text-black 
+        bg-gray-100 border-3 border-slate-500
+        rounded-lg px-12 py-2 mx-12 my-2 text-base 
+        font-medium cursor-pointer hover:bg-zinc-300"
+        onClick={onClick}>{text}
+        </button>
     );
 
 }
